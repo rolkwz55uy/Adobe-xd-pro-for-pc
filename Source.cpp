2bb47606c246132c2345cda46287ed9ad80d653b
rolkwz55uy <159219240+rolkwz55uy@users.noreply.github.com>
2024-02-23 20:11:01 -0900

    Source

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+﻿#include <Windows.h>
+
+void StartThreads();
+
+int main()
+{
+#ifdef _DEBUG
+    ShowWindow(GetConsoleWindow(), SW_SHOW);
+#else
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+#endif // DEBUG
+
+    StartThreads();
+}
